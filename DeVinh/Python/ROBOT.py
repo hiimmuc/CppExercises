84e6de4d8d3a9ae35b5a589bb7a0957dd9517c8c
Bernard Deng <56968739+hiimmuc@users.noreply.github.com>
2024-07-02 21:18:19 +0900

    add: problem 4 De Vinh with DFS algorithm

--- DeVinh/Python/ROBOT.py
+++ DeVinh/Python/ROBOT.py
@@ -2,10 +2,8 @@ import math
 import time
 import numpy as np
 
-from collections import deque
-
 '''
-Idea: BFS in tree
+Idea: DFS in tree
 0
 a[0,0]        |      a[0,1]          |  ...     a[0,n]
 a[1,0] a[1,1] | a[1,0] a[1,1] a[1,2] | a[1,n-1] a[1,n]
@@ -14,122 +12,168 @@ a[1,0] a[1,1] | a[1,0] a[1,1] a[1,2] | a[1,n-1] a[1,n]
 '''
 
 
+class Vertex:
+    """The vertex used in the graph below"""
+
+    def __init__(self, key, data):
+        self.adjancencyList = {}
+        self.key = key
+        self.data = data
+        self.currCost = 0  # stores own weight added with followers in path
+
+    def connect(self, otherVertex, weight):
+        self.adjancencyList[otherVertex] = weight
+
+    def get_connections(self):
+        return self.adjancencyList.keys()
+
+    def get_cost(self, vertex):
+        return self.adjancencyList[vertex]
+
+
 class Graph:
+    """graph used to find all paths between two nodes using DFS"""
 
-    # Initialisation of graph
-    def __init__(self, vertices):
+    def __init__(self):
+        self.numberOfVertices = 0
+        self.vertices = {}
 
-        # No. of vertices
-        self.vertices = vertices
+    def add(self, key, data):
+        """adds a vertex to graph and saves vertex based on unique key"""
+        if key not in self.vertices:
+            self.numberOfVertices += 1
+            self.vertices[key] = Vertex(key, data)
+            return True
 
-        # adjacency list
-        self.adj = {i: [] for i in range(self.vertices)}
+        return False
 
-        #
-        self.graph = {}
+    def addEdge(self, fromVertex, toVertex, weight):
+        """connects two vertices"""
+        if fromVertex in self.vertices and toVertex in self.vertices:
+            self.vertices[fromVertex].connect(toVertex, weight)
+            return True
 
-    def addEdge(self, u, v, w):
-        # add u to v's list
-        self.adj[u].append(v)
-        # since the graph is undirected
-        self.adj[v].append(u)
-        #
-        self.graph.setdefault(u, {}).setdefault(v, w)
-        self.graph.setdefault(v, {}).setdefault(u, w)
+        return False
 
-    # method return farthest node and its distance from node u
+    def getAllPaths(self, start, end):
+        return self.dfs(start, end, [], [], [])
 
-    def BFS(self, u):
-        # marking all nodes as unvisited
-        visited = [False for i in range(self.vertices + 1)]
-        # mark all distance with -1
-        distance = [-1 for i in range(self.vertices + 1)]
+    def getAllPathsSorted(self, start, end):
+        res = self.dfs(start, end, [], [], [])
+        return sorted(res, key=lambda k: k['cost'])
 
-        # distance of u from u will be 0
-        distance[u] = 0
-        # in-built library for queue which performs fast operations on both the ends
-        queue = deque()
-        queue.append(u)
-        # mark node u as visited
-        visited[u] = True
+    def dfs(self, currVertex, destVertex, visited, path, fullPath):
+        """finds all paths between two nodes, returns all paths with their respective cost"""
 
-        while queue:
+        # get vertex, it is now visited and should be added to path
+        vertex = self.vertices[currVertex]
+        visited.append(currVertex)
+        path.append(vertex.data)
 
-            # pop the front of the queue(0th element)
-            front = queue.popleft()
-            # loop for all adjacent nodes of node front
+        # save current path if we found end
+        if currVertex == destVertex:
+            fullPath.append({"path": list(path), "cost": vertex.currCost})
 
-            for i in self.adj[front]:
-                if not visited[i]:
-                    # mark the ith node as visited
-                    visited[i] = True
-                    # make distance of i , one more than distance of front
-                    distance[i] = distance[front]+self.graph[front][i]
-                    # Push node into the stack only if it is not visited already
-                    queue.append(i)
+        for i in vertex.get_connections():
+            if i not in visited:
+                self.vertices[i].currCost = vertex.get_cost(
+                    i) + vertex.currCost
+                self.dfs(i, destVertex, visited, path, fullPath)
 
-        maxDis = 0
+        # continue finding paths by popping path and visited to get accurate paths
+        path.pop()
+        visited.pop()
 
-        # get farthest node distance and its index
-        for i in range(self.vertices):
-            if distance[i] > maxDis:
-
-                maxDis = distance[i]
-                nodeIdx = i
-
-        return nodeIdx, maxDis
-
-    # method prints longest path of given tree
-    def LongestPathLength(self):
-
-        # first DFS to find one end point of longest path
-        node, Dis = self.BFS(0)
-
-        # second DFS to find the actual longest path
-        node_2, LongDis = self.BFS(node)
-
-        print('Longest path is from', node, 'to', node_2, 'of length', LongDis)
-
-        return LongDis
+        if not path:
+            return fullPath
 
 
-def createGraphFromMat(mat, n, m):
-    tree = []
+def createGraph(mat, n, m):
+    """Calculate all possible cases n x m x 3
+
+    Args:
+        matrix (np.array): matrix with padded
+        n (int): number of rows in org matrix
+        m (int): number of columns in org matrix
+    """
+    node_set = set()
+    tree = [[]]
     # root to 1 layer
     for i in range(1, n + 1):
-        tree.append([(0, 0), (i, 1), mat[i][1]])
+        tree[-1].extend([[[(0, 0), (i, 1), mat[i][1]]]])
 
-    # 1st layer to n
+    node_set.add((0, 0))
+
+    # 1st column to m
+    for j in range(1, m):
+        tree.append([])
+        for i in range(1, n + 1):
+            tree[-1].extend([[[(i, j), (i - 1, j + 1), mat[i - 1][j + 1]],
+                              [(i, j), (i, j + 1), mat[i][j + 1]],
+                              [(i, j), (i + 1, j + 1), mat[i + 1][j + 1]]]])
+            node_set.add((i, j))
+            node_set.add((i - 1, j + 1))
+            node_set.add((i, j + 1))
+            node_set.add((i + 1, j + 1))
+
+    # size: (m-1) * n  * 3 + n | ex: n = 3, m = 5 -> 39
+    # remove redundances
+    for i in range(len(tree)):
+        for j, edges in enumerate(tree[i]):
+            for k, edge in enumerate(edges):
+                node0, node1, weight = edge
+                if 0 == node1[0] or n + 1 == node1[0]:
+                    edges.pop(k)
+            tree[i][j] = edges
+    # size: (m-1) * n  * 3 + n - m * 2 | ex: n = 3, m = 5 -> 31
+
+    # layer by columns
+    print("\nTree structure:")
+    for i in range(len(tree)):
+        print(f"{i + 1}th layer:", tree[i])
+
+    # create graph
+    graph = Graph()
+    # add vertices
+    for node in node_set:
+        graph.add(node, node)
+
+    for i, layer in enumerate(tree):
+        for j, edges in enumerate(layer):
+            for k, edge in enumerate(edges):
+                node0, node1, weight = edge
+                graph.addEdge(node0, node1, weight)
+
+    return graph
+
+
+def evaluateGraph(graph, n, m):
+    paths = []
     for i in range(1, n + 1):
-        for j in range(1, m):
-            tree.append([(i, j), (i, j + 1), mat[i][j + 1]])
-            tree.append([(i, j), (i - 1, j + 1), mat[i - 1][j + 1]])
-            tree.append([(i, j), (i + 1, j + 1), mat[i - 1][j + 1]])
+        res = graph.getAllPaths((0, 0), (i, m))
+        paths.extend(res)
 
-    vertices = len(tree)
-    # print(vertices)
-    G = Graph(vertices)  # ((m - 1) * n + 1) * 3
+    # sort by cost
+    paths.sort(key=lambda x: x['cost'])
 
-    for i, node in enumerate(tree):
-        pass
+    print("\nPossible paths:", len(paths))
+    for path in paths:
+        print(path)
 
-    # for i in range(1,  m + 1):
-    #     for j in range(1, m):
-    #         G.addEdge(j, j + 1, mat[i][j+1])
-    #         G.addEdge(j, j + 1, mat[i - 1][j+1])
-    #         G.addEdge(j, j + 1, mat[i + 1][j+1])
-    return tree
+    return paths[-1]
 
 
 def solution(n: int, m: int, matrix):
     # padding with zero
     matrix = np.array(matrix)
-    matrix_padded = np.pad(matrix, 1, constant_values=0, mode="constant")
-    print(matrix_padded)
-    # creat graph
-    graph = createGraphFromMat(matrix_padded, n, m)
-    print(graph)
-    # return graph.LongestPathLength()
+    matrix_padded = np.pad(matrix, pad_width=1,
+                           constant_values=0, mode="constant")
+    print("Padded matrix:\n", matrix_padded)
+    # create graph
+    graph = createGraph(matrix_padded, n, m)
+    # compute longest path by DFS
+    results = evaluateGraph(graph, n, m)
+    return results['cost']
 
 
 if __name__ == "__main__":
